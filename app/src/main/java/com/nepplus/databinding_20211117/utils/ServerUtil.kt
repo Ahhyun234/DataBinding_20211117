2e11298b1a6005f02a6fd4358cff929c8bbb778c
이아현 <suk03029@gamil.com>
2021-11-17 16:54:03 +0900

    서버util 에서 인터페이스 추가함 - 본인이 받아온 jsonObj를 화면에 전달

--- app/src/main/java/com/nepplus/databinding_20211117/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/utils/ServerUtil.kt
@@ -7,29 +7,33 @@ import java.io.IOException
 
 class ServerUtil {
 
+    interface JsonResponseHandler {
+        fun onResponse(jsonObject: JSONObject)
+    }
+
     companion object {
 //        여기에 적는 변수/ㅎ마수는 Static에 대응됨
 //        클래스 이름.기능() 활용 가능
 
 //        모든 서버의 기능이 공유 할 Host 서버 컴퓨터 주소
 
-        val HOST_URL="http://54.180.52.26"
+        val HOST_URL = "http://54.180.52.26"
 
 //        로그인 함수 - POST
 
-        fun postRequestLogIN(email:String, pw:String){
+        fun postRequestLogIN(email: String, pw: String, handler: JsonResponseHandler?) {
 //            1. 어디로 가야 하는가 URL
             val urlString = "${HOST_URL}/user"
-            
+
 //            2. 어떤 데이터를 들고가는가? (파라미터)
             val formData = FormBody.Builder()
-                .add("email",email)
-                .add("password",pw)
+                .add("email", email)
+                .add("password", pw)
                 .build()
-            
+
 //            3. 어떤 메소드 + 1/2 데이터 결합 => 어떤 요청인지 완성
 //              어디로 어떤 방식으로 갈것인지
-            
+
             val request = Request.Builder()
                 .url(urlString)
                 .post(formData)
@@ -38,7 +42,7 @@ class ServerUtil {
 //            4. 완성된 Requst를 실제로 호출 => 클라이언트 역할
 
             val client = OkHttpClient()
-            client.newCall(request).enqueue( object :Callback{
+            client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
 
 //                    실패: 물리적으로 (연결 자체)접속 실패 -> 보통 토스트 띄움
@@ -54,17 +58,16 @@ class ServerUtil {
 //                    일반 String -> JsonObject로 변환 (한글도 원상복구)
                     val jsonObj = JSONObject(bodyString)
 
-                    Log.d("서버응답",jsonObj.toString())
+                    Log.d("서버응답", jsonObj.toString())
 
+//                    나를 호출한 화면에게 jsonObj를 처리하는 일처리를 미루자
+                    handler?.onResponse(jsonObj)
 
                 }
 
             })
 
 
-            
-            
-            
         }
 
 
