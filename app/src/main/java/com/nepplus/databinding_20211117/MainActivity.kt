62c099cda47624aeaf770c5680c278d901b56d3d
이아현 <suk03029@gamil.com>
2021-11-17 16:57:49 +0900

    메인 화면의 입장에서 서버의 응답을 JsonObj 변수에 담아서 확인

--- app/src/main/java/com/nepplus/databinding_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/MainActivity.kt
@@ -2,9 +2,11 @@ package com.nepplus.databinding_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.nepplus.databinding_20211117.databinding.ActivityMainBinding
 import com.nepplus.databinding_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -25,7 +27,14 @@ class MainActivity : BaseActivity() {
             val inputPW = binding.edtPW.text.toString()
 
 //            서버에 이메일/비번이 맞는 계정인지 로그인 요청
-                ServerUtil.postRequestLogIN(inputEmail,inputPW)
+                ServerUtil.postRequestLogIN(inputEmail,inputPW, object:ServerUtil.JsonResponseHandler{
+                    override fun onResponse(jsonObject: JSONObject) {
+
+//                        이 response가 main입장에서는 로그인 API를 호출하고 JsonObj를 받아서 돌아온 상황이기 때문에
+                        Log.d("화면에서의 obj",jsonObject.toString())
+                    }
+
+                })
 
 
              }
