a5c0aba4d91b93fdeb2ada3ec03a05816a102c0e
이아현 <suk03029@gamil.com>
2021-11-17 14:50:52 +0900

    databinding 변수 생성 및 초기화

--- app/src/main/java/com/nepplus/databinding_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/MainActivity.kt
@@ -2,10 +2,18 @@ package com.nepplus.databinding_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.databinding_20211117.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+
     }
 }
