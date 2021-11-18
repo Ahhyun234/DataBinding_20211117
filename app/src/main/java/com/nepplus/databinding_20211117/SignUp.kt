e418114b62ef677c4a92d9127ac5d3fb66aeb5e9
이아현 <suk03029@gamil.com>
2021-11-18 10:17:27 +0900

    서버의 응답을 분석 -> code 가 200이 아니면 서버가 실패. 왜 실패인지 내용이 나오도록 함

--- app/src/main/java/com/nepplus/databinding_20211117/SignUp.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/SignUp.kt
@@ -2,9 +2,12 @@ package com.nepplus.databinding_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.nepplus.databinding_20211117.databinding.ActivityMainBinding
 import com.nepplus.databinding_20211117.databinding.ActivitySignUpBinding
+import com.nepplus.databinding_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUp : BaseActivity() {
 
@@ -30,7 +33,25 @@ class SignUp : BaseActivity() {
 
 //            서버의 회원가입 기능에 전달(request)/ 돌아온 응답 대응(response)
 
+            ServerUtil.putRequestSignUp(inputEmail,inputPw,inputNickname,object :ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    
+                    val code = jsonObj.getInt("code")
+                    val message = jsonObj.getString("message")
+                    if (code==200){
+                        Toast.makeText(mContext, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+//                        실패-> 서버가 알려주는 실패 사유를 알려주자 
+                    }
+                        
 
+                }
+
+            })
 
         }
 
