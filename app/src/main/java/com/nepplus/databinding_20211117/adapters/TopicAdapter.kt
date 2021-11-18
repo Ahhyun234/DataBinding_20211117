4cd2da9640c8a6dc1e2ee3a26b4d0440d6828906
이아현 <suk03029@gamil.com>
2021-11-18 15:39:14 +0900

    토론 주제 어댑터 완성

--- app/src/main/java/com/nepplus/databinding_20211117/adapters/TopicAdapter.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/adapters/TopicAdapter.kt
@@ -1,8 +1,28 @@
 package com.nepplus.databinding_20211117.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.databinding_20211117.R
 import com.nepplus.databinding_20211117.datas.TopicData
 
-class TopicAdapter(val mContext:Context, val resId:Int, val mList:List<TopicData>):ArrayAdapter<TopicData>(mContext,resId,mList) {
+class TopicAdapter(val mContext: Context, val resId: Int, val mList: List<TopicData>) :
+    ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+        val row = tempRow!!
+        return row
+
+    }
+
+
 }
