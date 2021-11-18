a73b4125858f5aaf89b0a37a5e73acb309d25017
이아현 <suk03029@gamil.com>
2021-11-18 12:21:30 +0900

    컨텍스트 유틸에 저장된 토큰값 조회

--- app/src/main/java/com/nepplus/databinding_20211117/utils/Context.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/utils/Context.kt
@@ -2,24 +2,33 @@ package com.nepplus.databinding_20211117.utils
 
 class Context {
 
-    companion object{
-    private val prefName = "ColosseumPref"
-    private val TOKEN = "TOKEN"
+    companion object {
+        private val prefName = "ColosseumPref"
+        private val TOKEN = "TOKEN"
 
 //        token값에 대한 setter
 
-        fun setToken(context: Context, token:String){
+        fun setToken(context: Context, token: String) {
 //                        1. prefName을 이용해서 shaedpreference열기
-            val pref = context.getSharedPreferences(prefName,Context.MODE_PRIVATE)
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
 //                      2. 토큰값을 위에 저장
-                pref.edit().putString(TOKEN,token).apply()
-
+            pref.edit().putString(TOKEN, token).apply()
 
 
         }
 
 //        token 값에 대한 getter
+
+        fun getToken(context: Context): String {
+//            1. 메모장 열기
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//                2. 저장된 토큰 값을 리턴
+            return pref.getString(TOKEN,"")!!
+
+
+
+        }
 
     }
 }
