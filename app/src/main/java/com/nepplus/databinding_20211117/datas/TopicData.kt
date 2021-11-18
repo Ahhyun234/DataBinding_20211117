65425484edaa742d4d043529214f6162b6d5458b
이아현 <suk03029@gamil.com>
2021-11-18 15:18:12 +0900

    서버가 내려주는 토론주제 데이터를 담기위한 데이터 표현용 클래스 추가   - 임시: id, 제목, 이미지주소

---
+++ app/src/main/java/com/nepplus/databinding_20211117/datas/TopicData.kt
@@ -0,0 +1,12 @@
+package com.nepplus.databinding_20211117.datas
+
+class TopicData {
+
+//    생성자는 기본 생성자를 유지시키고 멤버변수만 추가 해서 제이슨 파싱해서 변수에 채워넣자
+//    멤버변수: 서버의 데이터를 보고 담아주는 용도의 변수들로 만들자
+
+    var id =0 //int가 들어올 자리
+    var title = "" //String이 들어올 자리
+    var imageUrl = ""
+
+}
