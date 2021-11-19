11d09ab7d3a133dfc7a5c2edcc54041ffdc4a82c
이아현 <suk03029@gamil.com>
2021-11-19 11:10:27 +0900

    댓글 데이터를 표현하는 클래스 추가 및 파싱 함수 추가

---
+++ app/src/main/java/com/nepplus/databinding_20211117/datas/ReplyData.kt
@@ -0,0 +1,24 @@
+package com.nepplus.databinding_20211117.datas
+
+import org.json.JSONObject
+
+class ReplyData {
+
+    var id = 0
+    var content = ""
+
+    companion object{
+
+        fun getReplyDataFromJson(jsonObject: JSONObject): ReplyData{
+
+            val replyData = ReplyData()
+
+            replyData.id = jsonObject.getInt("id")
+            replyData.content=jsonObject.getString("content")
+
+
+            return ReplyData()
+        }
+
+    }
+}
