6496d164b1094593fcc21e14ca4d61b7e862243d
이아현 <suk03029@gamil.com>
2021-11-19 10:54:52 +0900

    ㅌ표 완료 후에 리스폰스 대응  - 투표실패 시, 토스트만 출력  - 투표 성공 시 득표 현황 불러오기   => 이전에 만든 함수 재활용

--- app/src/main/java/com/nepplus/databinding_20211117/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/ViewTopicDetailActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.databinding_20211117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.nepplus.databinding_20211117.adapters.TopicAdapter
@@ -12,13 +13,13 @@ import com.nepplus.databinding_20211117.utils.ServerUtil
 import org.json.JSONObject
 
 class ViewTopicDetailActivity : BaseActivity() {
-    lateinit var binding:ActivityViewTopicDetailBinding
+    lateinit var binding: ActivityViewTopicDetailBinding
 
-    lateinit var mTopicData : TopicData
+    lateinit var mTopicData: TopicData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =DataBindingUtil.setContentView(this,R.layout.activity_view_topic_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
 
         setupEvent()
         setValues()
@@ -29,12 +30,29 @@ class ViewTopicDetailActivity : BaseActivity() {
 
         binding.btnVote01.setOnClickListener {
 //            첫진영에 투표 -> 서버 api 호출 -> 새로고침
-            ServerUtil.postRequestLogVote(mContext,mTopicData.sideList[0].id, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObject: JSONObject) {
+            ServerUtil.postRequestLogVote(
+                mContext,
+                mTopicData.sideList[0].id,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObject: JSONObject) {
 
-                }
+                        val code = jsonObject.getInt("code")
+                        if (code == 200) {
+//                            새로고침(토론 상세 조회 -> 선택진영목록-> 득표수)
+                            getTopicDetailFromServer()
 
-            })
+
+                        } else {
+                            val message = jsonObject.getString("message")
+
+                            runOnUiThread{
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    }
+
+                })
 
         }
 
@@ -42,38 +60,41 @@ class ViewTopicDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
-        mTopicData = intent.getSerializableExtra("topic")as TopicData
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
 
-        binding.txtTopicTitle.text=mTopicData.title
+        binding.txtTopicTitle.text = mTopicData.title
         Glide.with(mContext).load(mTopicData.imageUrl).into(binding.imgTopic)
 
 //        현재 진행 상황을 조회하는 API를 호출 -> 토론 진영 목록/몇표 획득
         getTopicDetailFromServer()
     }
 
-    fun getTopicDetailFromServer(){
+    fun getTopicDetailFromServer() {
 
-        ServerUtil.getRequestTopicDetail(mContext,mTopicData.id,object :ServerUtil.JsonResponseHandler{
-            override fun onResponse(jsonObject: JSONObject) {
+        ServerUtil.getRequestTopicDetail(
+            mContext,
+            mTopicData.id,
+            object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObject: JSONObject) {
 
-                val dataObj = jsonObject.getJSONObject("data")
-                val topicObj = dataObj.getJSONObject('topic')
+                    val dataObj = jsonObject.getJSONObject("data")
+                    val topicObj = dataObj.getJSONObject('topic')
 
 //                topic 토론 주제가 담긴 json Object 토픽 데이타변환 함수의 재료로 사용
-                mTopicData TopicData.getTopicDataFromJson(topicObj)
+                    mTopicData TopicData . getTopicDataFromJson (topicObj)
 
-                runOnUiThread{
-                    refreshUi()
+                    runOnUiThread {
+                        refreshUi()
+                    }
+
                 }
 
-                            }
-
-        })
+            })
 
 
     }
 
-    fun refreshUi(){
+    fun refreshUi() {
 //        mtopic 데이터 변경 시 리프레시
 
         binding.txtTopicTitle.text = mTopicData.title
