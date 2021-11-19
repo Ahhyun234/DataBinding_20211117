6b8641dbe79715768902e4254bee711ef402a763
이아현 <suk03029@gamil.com>
2021-11-19 10:15:14 +0900

    토론 주제를 추가 파싱 진영 목록 jSonArray를 같이 파싱 멤버변수에 ArrayList추가 -> 담아주기

--- app/src/main/java/com/nepplus/databinding_20211117/datas/TopicData.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/datas/TopicData.kt
@@ -13,20 +13,32 @@ class TopicData :Serializable {
     var imageUrl = ""
     var replyCount = 0
 
+//    토론 주제의 하위 목록 -> 선택 진영 sidedata 목록(ArrayList)
+    var sideList = ArrayList<Sidedata>()
+
+
     companion object{
 
 //        json Object를 가지고 TopicData형태로 변환해주는 함수 제작
 //        다른 화면들에서는 이 함수를 끌어다 사용
 
         fun getTopicDataFromJson(jsonObject: JSONObject):TopicData{
-            val resultTopicData = TopicData
+            val resultTopicData = TopicData()
             resultTopicData.id =jsonObject.getInt("id")
             resultTopicData.title = jsonObject.getString("title")
-            resultTopicData.imageUrl =jsonObject
-            resultTopicData.reply =jsonObject.getInt("reply_")
+            resultTopicData.imageUrl =jsonObject.getString("img_url")
+            resultTopicData.reply =jsonObject.getInt("reply_count")
 
-                    return
+//            토론주제 파싱 -> json array등장 -> 선택 진영 목록
 
+            val sidesArr = jsonObject.getJSONArray("sides")
+            for (i in 0 until sidesArr.length()){
+            val sideObj = sidesArr.getJSONObject(i)
+
+            val sidedata = Sidedata.getSideDataFromJson(sideObj)
+            resultTopicData.sideList.add(sidedata)
+            }
+                    return resultTopicData
         }
 
     }
