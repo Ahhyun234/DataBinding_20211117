84be618821ddcce009a5213b987b59b5a45cefeb
이아현 <suk03029@gamil.com>
2021-11-19 14:46:13 +0900

    파싱한 작성일지 재 가공해서 목록에 뿌려 줌

--- app/src/main/java/com/nepplus/databinding_20211117/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/adapters/ReplyAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.nepplus.databinding_20211117.R
 import com.nepplus.databinding_20211117.datas.ReplyData
 import com.nepplus.databinding_20211117.datas.TopicData
+import java.text.SimpleDateFormat
 
 class ReplyAdapter(val mContext: Context, val resId: Int, val mList: List<ReplyData>) :
     ArrayAdapter<ReplyData>(mContext, resId, mList) {
@@ -30,12 +31,15 @@ class ReplyAdapter(val mContext: Context, val resId: Int, val mList: List<ReplyD
         val txtReplycontent = row.findViewById<TextView>(R.id.txtReplyContent)
         val txtWriterNickName = row.findViewById<TextView>(R.id.txtWriterNickName)
         val txtSelectedSide = row.findViewById<TextView>(R.id.txtSelectedSide)
+        val txtCreatedAt = row.findViewById<TextView>(R.id.txtCreatedAt)
 
         txtWriterNickName.text=data.writer.nickname
         txtReplycontent.text=data.content
         txtSelectedSide.text = "( ${data.selectedSide.title} )"
 
+        val sdf = SimpleDateFormat("yyyy/MM/dd a h시 m분")
 
+        txtCreatedAt.text = sdf.format(data.createAt.time)
         return row
 
     }
