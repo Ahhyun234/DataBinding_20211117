41d67595a556e40b0852c1eac1599a6205e9c213
이아현 <suk03029@gamil.com>
2021-11-19 11:13:57 +0900

    어댑터 클래스 추가  - 복붙후 일부만 수정 함

---
+++ app/src/main/java/com/nepplus/databinding_20211117/adapters/ReplyAdapter.kt
@@ -0,0 +1,37 @@
+package com.nepplus.databinding_20211117.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.nepplus.databinding_20211117.R
+import com.nepplus.databinding_20211117.datas.ReplyData
+import com.nepplus.databinding_20211117.datas.TopicData
+
+class ReplyAdapter(val mContext: Context, val resId: Int, val mList: List<ReplyData>) :
+    ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.reply_list_item, null)
+        }
+        val row = tempRow!!
+
+        val data = mList[position]
+
+
+
+        return row
+
+    }
+
+
+}
