6f5852f86c5aea43224e7bf6d3db91847badcf46
이아현 <suk03029@gamil.com>
2021-11-22 10:21:06 +0900

    로그아웃 처리   = 토큰 삭제 -> 로그아웃   = 보이던 모든 화면 종료

--- app/src/main/java/com/nepplus/databinding_20211117/MyProfileActivity.kt
+++ app/src/main/java/com/nepplus/databinding_20211117/MyProfileActivity.kt
@@ -1,9 +1,13 @@
 package com.nepplus.databinding_20211117
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.nepplus.databinding_20211117.databinding.ActivityMyProfileBinding
+import com.nepplus.databinding_20211117.utils.ContextUtil
 
 class MyProfileActivity : BaseActivity() {
 
@@ -18,6 +22,26 @@ class MyProfileActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
+        binding.btnLogout.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃 확인")
+            alert.setMessage("정말 로그아웃 하시겠습니까")
+            alert.setPositiveButton.("확인", DialogInterface.OnClickListener({dialogInterface, i ->
+                //로그아웃 처리는 ? => 저장된 Token을 "" 으로 돌려주자 (내폰에 저장된 토큰 삭제)
+
+                ContextUtil.setToken(mContext,"")
+//            화면 종료 -> Splash로 이돌
+
+            val myIntent = Intent(mContext,SplashActivity::class.java)
+            myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(myIntent)
+
+
+            })
+            alert.setNegativeButton("취소",null)
+                alert.show()
+        }
 
     }
 
